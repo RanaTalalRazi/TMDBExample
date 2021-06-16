5657367daceff5282b105c2d280d85c709215f50
Rana Talal Razi <ranatalalrazi@gmail.com>
2021-06-17 01:59:00 +0500

    project create

---
+++ app/src/main/java/com/ranatalal/tmdbexample/views/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ranatalal.tmdbexample.views.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ranatalal.tmdbexample.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
